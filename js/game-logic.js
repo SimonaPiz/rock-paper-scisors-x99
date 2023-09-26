ba1dd67bc337d7d9660ddf8efd2155b2ca35078c
Simona Pizio <simona.pizio@gmail.com>
2023-09-26 15:27:04 +0200

    #3 - Function setPlayerMoves - Implemented and tested

--- js/game-logic.js
+++ js/game-logic.js
@@ -18,3 +18,58 @@ let playerTwoMoveThreeType;
 let playerTwoMoveOneValue;
 let playerTwoMoveTwoValue;
 let playerTwoMoveThreeValue;
+
+/*2.  Una funzione chiamata setPlayerMoves, che prenderà una stringa che rappresenta un giocatore 
+(nella forma di 'Player One'o 'Player Two'), tre tipi di mosse e tre valori di mossa e imposterà 
+le variabili di movimento globali corrette. La firma del metodo per questa funzione dovrebbe 
+essere la seguente: setPlayerMoves(player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, 
+moveThreeType, moveThreeValue). */
+
+const setPlayerMoves = (player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, 
+  moveThreeType, moveThreeValue) => {
+    if(
+      //if valid inputs and not missing
+      moveOneType && moveThreeType && moveTwoType && 
+      moveOneValue && moveTwoValue && moveThreeValue &&
+      // move values are more then 1
+      ((moveOneValue >= 1 && moveTwoValue >= 1 && moveThreeValue >= 1) &&
+      // move values are less then 99
+      (moveOneValue <= 99 && moveTwoValue <= 99 && moveThreeValue <= 99) ) &&
+      // values sum is less then 99
+      (moveOneValue + moveTwoValue + moveThreeValue) < 100 &&
+      //if a valid move type
+      (moveOneType.toLowerCase() === 'rock' || 
+      moveOneType.toLowerCase() === 'paper' ||
+      moveOneType.toLowerCase() === 'scissors') && 
+      (moveTwoType.toLowerCase() === 'rock' ||
+      moveTwoType.toLowerCase() === 'paper' ||
+      moveTwoType.toLowerCase() === 'scissors') &&
+      (moveThreeType.toLowerCase() === 'rock' ||
+      moveThreeType.toLowerCase() === 'paper' ||
+      moveThreeType.toLowerCase() === 'scissors')
+    ) {
+      // player one
+      if(player.toLowerCase() === 'player one') {
+        playerOneMoveOneType = moveOneType;
+        playerOneMoveTwoType = moveTwoType;
+        playerOneMoveThreeType = moveThreeType;
+        playerOneMoveOneValue = moveOneValue;
+        playerOneMoveTwoValue = moveTwoValue;
+        playerOneMoveThreeValue = moveThreeValue;
+      } 
+      //player two
+      else if (player.toLowerCase() === 'player two') {
+        playerTwoMoveOneType = moveOneType;
+        playerTwoMoveTwoType = moveTwoType;
+        playerTwoMoveThreeType = moveThreeType;
+        playerTwoMoveOneValue = moveOneValue;
+        playerTwoMoveTwoValue = moveTwoValue;
+        playerTwoMoveThreeValue = moveThreeValue;
+      } else {
+        //if invalid player
+        return;
+      }
+    } else {
+      return;
+    };
+  };
