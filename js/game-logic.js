d7e527cc18e1b1d1fbae154f133abd5d80d9a13b
Simona Pizio <simona.pizio@gmail.com>
2023-09-26 15:33:25 +0200

    #4 - Function getRaundWinner - Implemented and tested

--- js/game-logic.js
+++ js/game-logic.js
@@ -73,3 +73,145 @@ const setPlayerMoves = (player, moveOneType, moveOneValue, moveTwoType, moveTwoV
       return;
     };
   };
+
+  /*3.  Una funzione chiamata getRoundWinner, che prende un numero di round ( 1, 2, o 3), 
+confronta i tipi di mosse e i valori di entrambi i giocatori per quel round e restituisce 
+il vincitore appropriato ( 'Player One', 'Player Two', o 'Tie') */
+const getRoundWinner = (roundNum) => {
+  switch (roundNum) {
+    // round 1:
+    case 1:
+      // verifichiamo che gli input siano validi
+      if(
+        !playerOneMoveOneType || !playerOneMoveOneValue ||
+        !playerTwoMoveOneType || !playerTwoMoveOneValue
+      ) {
+        return null;
+      };
+
+      switch (playerOneMoveOneType) {
+        case playerTwoMoveOneType:
+          if (playerOneMoveOneValue < playerTwoMoveOneValue) {
+            return 'Player Two';
+          } else if (playerOneMoveOneValue > playerTwoMoveOneValue) {
+            return 'Player One';
+          } else {
+            return 'Tie';
+          }
+          break;
+        case 'rock':
+          if (playerTwoMoveOneType === 'paper') {
+            return 'Player Two';
+          } else {
+            return 'Player One';
+          }
+          break;
+        case 'paper':
+          if (playerTwoMoveOneType === 'scissors') {
+            return 'Player Two';
+          } else {
+            return 'Player One';
+          }
+          break;
+        case 'scissors':
+          if (playerTwoMoveOneType === 'rock') {
+            return 'Player Two';
+          } else {
+            return 'Player One';
+          }
+          break;
+      };
+      break;
+
+    // round 2:
+    case 2:
+      // verifichiamo che gli input siano validi
+      if(
+        !playerOneMoveTwoType || !playerOneMoveTwoValue ||
+        !playerTwoMoveTwoType || !playerTwoMoveTwoValue        
+      ) {
+        return null;
+      };
+
+      switch (playerOneMoveTwoType) {
+        case playerTwoMoveTwoType:
+          if (playerOneMoveTwoValue < playerTwoMoveTwoValue) {
+            return 'Player Two';
+          } else if (playerOneMoveTwoValue > playerTwoMoveTwoValue) {
+            return 'Player One';
+          } else {
+            return 'Tie';
+          }
+          break;
+        case 'rock':
+          if (playerTwoMoveTwoType === 'paper') {
+            return 'Player Two';
+          } else {
+            return 'Player One';
+          }
+          break;
+        case 'paper':
+          if (playerTwoMoveTwoType === 'scissors') {
+            return 'Player Two';
+          } else {
+            return 'Player One';
+          }
+          break;
+        case 'scissors':
+          if (playerTwoMoveTwoType === 'rock') {
+            return 'Player Two';
+          } else {
+            return 'Player One';
+          }
+          break;
+      };
+      break;
+
+    // round 3:
+    case 3:
+      // verifichiamo che gli input siano validi
+      if(
+        !playerOneMoveThreeType || !playerOneMoveThreeValue ||
+        !playerTwoMoveThreeType || !playerTwoMoveThreeValue        
+      ) {
+        return null;
+      };
+
+      switch (playerOneMoveThreeType) {
+        case playerTwoMoveThreeType:
+          if (playerOneMoveThreeValue < playerTwoMoveThreeValue) {
+            return 'Player Two';
+          } else if (playerOneMoveThreeValue > playerTwoMoveThreeValue) {
+            return 'Player One';
+          } else {
+            return 'Tie';
+          }
+          break;
+        case 'rock':
+          if (playerTwoMoveThreeType === 'paper') {
+            return 'Player Two';
+          } else {
+            return 'Player One';
+          }
+          break;
+        case 'paper':
+          if (playerTwoMoveThreeType === 'scissors') {
+            return 'Player Two';
+          } else {
+            return 'Player One';
+          }
+          break;
+        case 'scissors':
+          if (playerTwoMoveThreeType === 'rock') {
+            return 'Player Two';
+          } else {
+            return 'Player One';
+          }
+          break;
+      };
+      break;
+    default: return null;
+      break;
+  };
+};
+
