388bedef0aae005f0b9f9a58659661fe10dec9e8
Simona Pizio <simona.pizio@gmail.com>
2023-09-26 15:08:03 +0200

    #2 - Player moves - Implemented and tested

--- js/game-logic.js
+++ js/game-logic.js
@@ -1,1 +1,20 @@
 // All code should be written in this file.
+
+/*1.  Dodici variabili globali che rappresentano i tipi e i valori di mossa di ogni giocatore 
+(3 tipi di mossa e 3 valori di mossa per ogni giocatore). Questi nomi di variabili dovrebbero 
+essere sotto forma di playerOneMoveOneType, playerOneMoveOneValue, ecc. */
+let playerOneMoveOneType;
+let playerOneMoveTwoType;
+let playerOneMoveThreeType;
+
+let playerOneMoveOneValue;
+let playerOneMoveTwoValue;
+let playerOneMoveThreeValue;
+
+let playerTwoMoveOneType;
+let playerTwoMoveTwoType;
+let playerTwoMoveThreeType;
+
+let playerTwoMoveOneValue;
+let playerTwoMoveTwoValue;
+let playerTwoMoveThreeValue;
